5fd477238988c4ae29ad46210accb770733e73d7
WCramRTC <91401050+WCramRTC@users.noreply.github.com>
2024-04-02 19:05:14 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            // To get your project setup with github
+            // 1. Open View -> GitChanges
+
+
+            // "Hello, World"
+            // Double quotes tell the computer that everything in between is a STRING
+            Console.WriteLine("Hello, Mochi!");
+            
+            // Welcome to Programming 120
+            Console.Write("Welcome to ");
+            Console.WriteLine("Programming 120");
+
+
+            //Console.Write("Please enter you name: ");
+            //string userName = Console.ReadLine();
+            //Console.WriteLine("Welcome " + userName);
+
+
+
+            // To comment Code, do double forwad slashs //
+            // Console gives us access to working with the console
+            // CODE IS CASE SENSTIVE
+            // Dot, ., is used to access the different ways to interact with your code
+            // Parentheses are used to RUN our code, and pass in different arguments
+            // semi colon, the end of a single statement
+            // CUrly braces define code blocks
+
+            // Compile Time Error
+            // Is an error that visual studio will warn you about, that there is improper syntax someplace. Needs to be fixed before you can build your code.
+            // Errors in the Error list will "Help" you find the problem
+
+        } // End of Main
+
+    } // End of class
+
+} // ENd of namespace
